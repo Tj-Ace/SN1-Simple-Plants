bc6738400823084920aabe2097ab3e40530908b4
Tj-Ace <tgrommesh@gmail.com>
2026-06-11 17:17:42 -0500

    Delete obj directory

--- obj/Release/net472/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace SimplePlants
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "SimplePlants";
-        public const string PLUGIN_NAME = "SimplePlants";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
