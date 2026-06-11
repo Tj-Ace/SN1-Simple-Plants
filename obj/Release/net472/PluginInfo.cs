96bb940dfae2c6ef8a3af20ce977348103ac97b0
Tj-Ace <tgrommesh@gmail.com>
2026-06-11 17:16:59 -0500

    Add files via upload

---
+++ obj/Release/net472/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace SimplePlants
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "SimplePlants";
+        public const string PLUGIN_NAME = "SimplePlants";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
